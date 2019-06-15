07233821767aac090fa7fa948078d9f3940ce66e
Patrick Arengh <parengh.2018@gmail.com>
2019-06-14 19:01:29 -0700

    Testing again

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ int main()
   std::cout<<std::endl<<"First GitHub test program...";
   std::cout<<std::endl<<"Second GitHub test program...";
   std::cout<<std::endl<<"Third GitHub test program...";
-  std::cout<<std::endl<<"Testing GitHub test program...";
+  std::cout<<std::endl<<"Testing again GitHub test program...";
   return 0;
 }
 
